26a49c9f415fe9c0cbf3d9e6866dd102b892512d
dorotaro <dorota.rogoza13@gmail.com>
2021-12-21 17:51:00 +0200

    'Difficulty' enum changed to display as string (not numeral value) in the swagger interface

--- RestAPI_ToDoApp/Startup.cs
+++ RestAPI_ToDoApp/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Service;
+using System.Text.Json.Serialization;
 
 
 
@@ -48,6 +49,13 @@ namespace RestAPI_ToDoApp
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestAPI_ToDoApp", Version = "v1" });
             });
             services.AddDomain(Configuration);
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+            });
+                   
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
